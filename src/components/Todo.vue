46021dcec2e05a26797672a3a460f87e2246208b
Ha-ji-me <hjmx3599@gmail.com>
2022-04-14 16:18:33 +0900

    first-commit

---
+++ src/components/Todo.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+
+const todoItem = '勉強';
+const todoItem2 = '筋トレ';
+
+const url1 = 'https://www.doutor.co.jp/dcs/';
+const url2 = 'https://www.anytimefitness.co.jp/';
+
+const isComplete = (todoItem : string) => {
+    alert(todoItem + 'を本当に完了しましたか？')
+}
+
+</script>
+
+<template>
+    <hr>
+    <label>Todoリスト</label>
+    <hr>
+    <div>
+        <label class="todo">内容：{{todoItem}}</label>
+        <!-- <a href="https://www.doutor.co.jp/dcs/">開催場所</a> -->
+        <div>
+            開催場所：<a v-bind:href="url1">こちら</a>
+        </div>
+        <button v-on:click="isComplete(todoItem)">完了</button>
+    </div>
+    <div>
+        <label class="todo">内容：{{todoItem2}}</label>
+        <div>
+            開催場所：<a v-bind:href="url2">こちら</a>
+        </div>
+        <button v-on:click="isComplete(todoItem2)">完了</button>
+    </div>
+
+</template>
+
+<style scoped>
+label {
+    font-size: 80px;
+    font-weight: bold;
+    color: green;
+}
+.todo {
+    font-size:30px;
+    color:black;
+    margin-bottom:8px;
+
+}
+</style>
---
+++ src/components/TodoCard.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+    
+</script>
+
+<template>
+    <div>
+        <p>・課題とテスト勉強</p>
+    </div>
+</template>
+
+<style scoped>
+p {
+    color:red;
+    font-size: 30px;
+}
+</style>
