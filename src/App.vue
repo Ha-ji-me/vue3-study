c44a78053dad72493a4f9a6eb9b9e4f82a12c0e8
Ha-ji-me <hjmx3599@gmail.com>
2022-04-21 21:25:05 +0900

    'コンポーネント受け渡しテスト'

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-
+import Persons from './components/Persons.vue'
 
 </script>
 
@@ -9,15 +9,17 @@
 <template>
   <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
   <!-- <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" /> -->
+  <p>App.vueページです</p>
+  <Persons />
 </template>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
---
+++ src/components/Persons.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import PersonPostForm from './PersonPostForm.vue';
+import PersonList from './PersonList.vue';
+</script>
+
+<template>
+<p>Persons.vueです</p>
+<div class="container">
+    <div class="form-container">
+        <PersonPostForm />
+    </div>
+    <div class="list-container">
+        <ul>
+            <PersonList />
+        </ul>
+    </div>
+</div>
+
+</template>
+
+<style>
+</style>
