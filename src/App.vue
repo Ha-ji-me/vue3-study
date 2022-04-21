9b353c6ce7034e5f604e71e25507b7ecde3ca2ee
Ha-ji-me <hjmx3599@gmail.com>
2022-04-21 21:10:46 +0900

    reset-file

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-import Todo from './components/Todo.vue'
-import TodoCard from './components/TodoCard.vue'
-import Tweet from './components/Tweet.vue'
+
 
 </script>
 
@@ -12,8 +9,6 @@ import Tweet from './components/Tweet.vue'
 <template>
   <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
   <!-- <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" /> -->
-  <!-- <Todo /> -->
-  <Tweet />
 </template>
 
 <style>
