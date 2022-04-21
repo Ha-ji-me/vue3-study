95cb465c3af3d22902da0bb8ec5a855a114566ee
Ha-ji-me <hjmx3599@gmail.com>
2022-04-21 22:00:10 +0900

    'フレーム作成'

--- src/components/PersonPostForm.vue
+++ src/components/PersonPostForm.vue
@@ -1,9 +1,46 @@
 <script setup lang="ts">
+const addForm = () => {
+    alert('追加しますか？')
+}
+const addName = () => {
+    
+}
+const addAge = () => {
+    
+}
 </script>
 
 <template>
-<p>PersonPostFormです</p>
+<div class="form-container">
+    <p>PersonPostFormです</p>
+    <div class="name-column">
+        name：<input :bind="addName">
+    </div>
+    <div class="age-column">
+        age：<input :bind="addAge">
+    </div>
+    <div class="add-button">
+        <button @click="addForm">追加</button>
+    </div>
+</div>
 </template>
 
 <style scoped>
+.form-container {
+    margin: 50px;
+    padding: 70px;
+    text-align: center; 
+    border: solid rgba(122, 86, 50, .6);
+    border-radius: 10px;
+}
+.name-column {
+    padding:2px;
+}
+.age-column {
+    padding:2px;
+}
+.add-button {
+    padding:5px;
+}
+
 </style>
